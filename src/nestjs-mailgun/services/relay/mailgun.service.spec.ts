a627e330576dbefa7a1d0d3617459c777f6012a4
Nuno Carvalhão <nuno.carvalhao@gmail.com>
2021-11-28 11:34:41 +0000

    feat: add tests to workflow pipeline

--- src/nestjs-mailgun/services/relay/mailgun.service.spec.ts
+++ src/nestjs-mailgun/services/relay/mailgun.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MAILGUN_CONFIGURATION } from '../../../nestjs-mailgun/tokens/tokens';
 import { MailgunService } from './mailgun.service';
 
 describe('MailgunService', () => {
@@ -6,7 +7,17 @@ describe('MailgunService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailgunService],
+      providers: [
+        MailgunService,
+        {
+          provide: MAILGUN_CONFIGURATION,
+          useValue: {
+            username: 'Nuno',
+            key: process.env.MAILGUN_KEY,
+            // url: 'api.eu.mailgun.net',
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MailgunService>(MailgunService);
@@ -15,4 +26,21 @@ describe('MailgunService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('Send email', () => {
+    return expect(
+      service.createEmail(process.env.MAILGUN_DOMAIN, {
+        from: 'package@test.com',
+        subject: 'TEST',
+        to: 'benfica000@gmail.com',
+        text: 'Test was successful',
+      }),
+    ).resolves.toBeDefined();
+  });
+
+  // it('Validate email', () => {
+  //   return expect(
+  //     service.validateEmail('nuno.carvalhao@gmail.com'),
+  //   ).resolves.toBeDefined();
+  // });
 });
--- src/nestjs-mailgun/services/relay/mailgun.service.ts
+++ src/nestjs-mailgun/services/relay/mailgun.service.ts
@@ -5,8 +5,19 @@ import Client from 'mailgun.js/dist/lib/client';
 import Options from 'mailgun.js/dist/lib/interfaces/Options';
 import { MailgunEmailModel } from '../../../nestjs-mailgun/classes/mailgun-email-model';
 import FormData from 'form-data';
-import { CreateUpdateList, DestroyedList, MailingList } from 'mailgun.js/dist/lib/interfaces/lists';
-import { CreateUpdateMailListMembers, DeletedMember, MailListMember, MailListMembersQuery, MultipleMembersData, NewMultipleMembersResponse } from 'mailgun.js/dist/lib/interfaces/mailListMembers';
+import {
+  CreateUpdateList,
+  DestroyedList,
+  MailingList,
+} from 'mailgun.js/dist/lib/interfaces/lists';
+import {
+  CreateUpdateMailListMembers,
+  DeletedMember,
+  MailListMember,
+  MailListMembersQuery,
+  MultipleMembersData,
+  NewMultipleMembersResponse,
+} from 'mailgun.js/dist/lib/interfaces/mailListMembers';
 
 export interface EmailOptions {
   from: string;
@@ -21,38 +32,69 @@ export interface EmailOptions {
 @Injectable()
 export class MailgunService {
   private readonly mailgun: Client;
-  constructor(@Inject(MAILGUN_CONFIGURATION) private readonly configuration: Options) {
-    this.mailgun = new Mailgun(FormData).client(configuration)
+  constructor(
+    @Inject(MAILGUN_CONFIGURATION) private readonly configuration: Options,
+  ) {
+    this.mailgun = new Mailgun(FormData).client(configuration);
   }
 
+  public createEmail = async (
+    domain: string,
+    data: EmailOptions | MailgunEmailModel,
+  ): Promise<any> => this.mailgun.messages.create(domain, data);
 
-  public createEmail = async (domain: string, data: EmailOptions | MailgunEmailModel): Promise<any> =>
-    this.mailgun.messages.create(domain, data);
+  public validateEmail = async (
+    email: string,
+  ): Promise<{
+    address: string;
+    did_you_mean: string;
+    is_valid: boolean;
+    parts: { display_name: string; domain: string; local_part: string };
+  }> => this.mailgun.validate.get(email);
 
-  public validateEmail = async (email: string): Promise<boolean> =>
-    this.mailgun.validate.get(email);
-
-  public createList = async(data :CreateUpdateList) :Promise<MailingList> =>
+  public createList = async (data: CreateUpdateList): Promise<MailingList> =>
     this.mailgun.lists.create(data);
-   public destroyList = async(mailListAddress :string) :Promise<DestroyedList> =>
-    this.mailgun.lists.destroy(mailListAddress)
-  
-    public getList = async(mailListAddress :string) :Promise<MailingList> =>
-    this.mailgun.lists.get(mailListAddress);
-    public updateList = async(mailListAddress :string, data: CreateUpdateList) :Promise<MailingList> =>
-    this.mailgun.lists.update(mailListAddress,data);
 
-    public listAddMember = async(mailListAddress: string, data: CreateUpdateMailListMembers) :Promise<MailListMember> =>
+  public destroyList = async (
+    mailListAddress: string,
+  ): Promise<DestroyedList> => this.mailgun.lists.destroy(mailListAddress);
+
+  public getList = async (mailListAddress: string): Promise<MailingList> =>
+    this.mailgun.lists.get(mailListAddress);
+
+  public updateList = async (
+    mailListAddress: string,
+    data: CreateUpdateList,
+  ): Promise<MailingList> => this.mailgun.lists.update(mailListAddress, data);
+
+  public listAddMember = async (
+    mailListAddress: string,
+    data: CreateUpdateMailListMembers,
+  ): Promise<MailListMember> =>
     this.mailgun.lists.members.createMember(mailListAddress, data);
 
-    public listGetMembers = async(mailListAddress: string, query?: MailListMembersQuery) :Promise<MailListMember[]> =>
+  public listGetMembers = async (
+    mailListAddress: string,
+    query?: MailListMembersQuery,
+  ): Promise<MailListMember[]> =>
     this.mailgun.lists.members.listMembers(mailListAddress, query);
 
-    public listCreateMembers = async(mailListAddress: string, data: MultipleMembersData) :Promise<NewMultipleMembersResponse> =>
+  public listCreateMembers = async (
+    mailListAddress: string,
+    data: MultipleMembersData,
+  ): Promise<NewMultipleMembersResponse> =>
     this.mailgun.lists.members.createMembers(mailListAddress, data);
-    public listupdateMember = async(address: string,memberAddress: string, data: CreateUpdateMailListMembers) :Promise<MailListMember> =>
-    this.mailgun.lists.members.updateMember(address,memberAddress, data);
-    public listDestroyMember = async(address: string,memberAddress: string,) :Promise<DeletedMember> =>
-    this.mailgun.lists.members.destroyMember(address,memberAddress);
 
-} 
+  public listupdateMember = async (
+    address: string,
+    memberAddress: string,
+    data: CreateUpdateMailListMembers,
+  ): Promise<MailListMember> =>
+    this.mailgun.lists.members.updateMember(address, memberAddress, data);
+
+  public listDestroyMember = async (
+    address: string,
+    memberAddress: string,
+  ): Promise<DeletedMember> =>
+    this.mailgun.lists.members.destroyMember(address, memberAddress);
+}
